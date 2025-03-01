d0832d0b78b363786ea4315e1c0983f31c52f8db
Jeff Brown <jeff.brown@gmail.com>
2025-02-28 19:08:21 -0800

    Implement the IR transmitter function.
    Tested synthetically with a round-trip through the decoder.

--- components/maxxfan_protocol/maxxfan_protocol.cpp
+++ components/maxxfan_protocol/maxxfan_protocol.cpp
@@ -7,12 +7,62 @@ namespace remote_base {
 
 static const char *const TAG = "remote.maxxfan";
 
+constexpr uint8_t PREAMBLE[] = { 0x5a, 0xa5, 0x80, 0x7f, 0x40, 0xbf, 0x20, 0xdf, 0x10, 0xcc };
+constexpr int32_t BIT_TIME_US = 800;
+
+void MaxxfanProtocol::encode(RemoteTransmitData *dst, const MaxxfanData &data) {
+  uint8_t packet[16];
+  memcpy(packet, PREAMBLE, sizeof(PREAMBLE));
+  packet[10] = 0 |
+      (data.fan_on ? 0x01 : 0) |
+      (data.special ? 0x02 : 0) |
+      (data.fan_exhaust ? 0x04 : 0) |
+      (data.cover_open ? 0x08 : 0) |
+      (data.auto_mode ? 0x10 : 0) |
+      (data.warn ? 0x20 : 0);
+  packet[11] = data.fan_speed;
+  packet[12] = data.auto_temperature;
+  packet[13] = 0xff;
+  packet[14] = 0x23;
+  packet[15] = packet[10] ^ packet[11] ^ packet[12] ^ packet[13] ^ packet[14];
+
+  dst->set_carrier_frequency(38000);
+  dst->reserve(10 * sizeof(packet) + 1);
+
+  for (size_t i = 0; i < sizeof(packet); i++) {
+    dst->mark(BIT_TIME_US);
+    uint8_t byte = packet[i];
+    for (uint32_t j = 0; j < 8; j++) {
+      if (byte & 1) {
+        dst->space(BIT_TIME_US);
+      } else {
+        dst->mark(BIT_TIME_US);
+      }
+      byte >>= 1;
+    }
+    dst->space(BIT_TIME_US * 2);
+  }
+  dst->space(BIT_TIME_US * 8);
+
+  if (false) { // FOR DEBUGGING
+    RemoteReceiveData src(dst->get_data(), 0, ToleranceMode::TOLERANCE_MODE_TIME);
+    auto round_trip = decode(src);
+    if (round_trip) {
+      dump(*round_trip);
+      if (!(*round_trip == data)) {
+        ESP_LOGE(TAG, "Round-trip decoding produced a different result than the original data");
+      }
+    } else {
+      ESP_LOGE(TAG, "Round-trip decoding failed");
+    }
+  }
+}
+
 class BitReader {
  public:
   BitReader(const RemoteReceiveData& data) : data_(data) {}
 
   int next_bit() {
-    constexpr int32_t BIT_TIME_US = 800;
     for (;;) {
       if (!data_.is_valid(index_)) return -1; // no more data
       const int32_t duration = data_[index_];
@@ -33,13 +83,8 @@ class BitReader {
   int32_t consumed_{};
 };
 
-
-void MaxxfanProtocol::encode(RemoteTransmitData *dst, const MaxxfanData &data) {
-    // NOT IMPLEMENTED
-}
-
 optional<MaxxfanData> MaxxfanProtocol::decode(RemoteReceiveData src) {
-  if (false) {
+  if (false) { // FOR DEBUGGING
     char buffer[256];
     size_t index = 0;
     BitReader reader(src);
@@ -66,7 +111,7 @@ optional<MaxxfanData> MaxxfanProtocol::decode(RemoteReceiveData src) {
   }
   if (reader.next_bit() != 1) return {}; // expected end of transmission
 
-  if (false) {
+  if (false) { // FOR DEBUGGING
     ESP_LOGD(TAG, "Received packet: %02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x,%02x",
         packet[0], packet[1], packet[2], packet[3],
         packet[4], packet[5], packet[6], packet[7],
@@ -74,8 +119,7 @@ optional<MaxxfanData> MaxxfanProtocol::decode(RemoteReceiveData src) {
         packet[12], packet[13], packet[14], packet[15]);
   }
 
-  constexpr uint8_t preamble[] = { 0x5a, 0xa5, 0x80, 0x7f, 0x40, 0xbf, 0x20, 0xdf, 0x10, 0xcc };
-  if (memcmp(packet, preamble, sizeof(preamble))) return {}; // preamble mismatch
+  if (memcmp(packet, PREAMBLE, sizeof(PREAMBLE))) return {}; // preamble mismatch
 
   const uint8_t state = packet[10];
   const uint8_t fan_speed = packet[11];
